039fba31182e4cd6ad426c72a5f902aa5b0e72ac
Marc Totaro <m.totaro@tecplot.com>
2024-04-12 15:23:53 -0500

    Home row mods

---
+++ keyboards/massdrop/alt/keymaps/Crambeary/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#define TAPPING_TERM 220
+#define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD
+
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+// #define AUTO_SHIFT_NO_SETUP
+
+#define RETRO_SHIFT 500
+
